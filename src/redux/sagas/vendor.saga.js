4cc012d45854cb88ab9508290442ceb67cfd1142
Lloyd Prock <lloyd.prock@gmail.com>
2022-11-14 11:14:12 -0600

    added vendor reducer and vendor saga base

---
+++ src/redux/sagas/vendor.saga.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+import { put, takeEvery } from "redux-saga/effects";
+
+// worker Saga: will be fired on "FETCH_USER" actions
+function* fetchVendors() {
+  try {
+    // Setup config for credentials
+    const config = {
+      headers: { "Content-Type": "application/json" },
+      withCredentials: true,
+    };
+    // Store response from axios
+    const response = yield axios.get("/api/vendors", config);
+    // Send response to reducer
+    yield put({ type: "SET_VENDORS", payload: response.data });
+  } catch (error) {
+    console.log("error caught in FETCH_VENDORS :>> ", error);
+  }
+}
+
+// worker Saga will fire on "FETCH_VENDORS"
+
+function* vendorsSaga() {
+  yield takeEvery("FETCH_VENDORS", fetchVendors);
+}
+
+export default vendorsSaga;
