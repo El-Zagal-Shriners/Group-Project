5e5acf030e054c0b0640ed13d79569c48c91abab
Lloyd <34199477+Lloydrp@users.noreply.github.com>
2022-11-14 11:50:12 -0600

    Update _root.reducer.js
    Same for import

--- src/redux/reducers/_root.reducer.js
+++ src/redux/reducers/_root.reducer.js
@@ -1,7 +1,7 @@
 import { combineReducers } from "redux";
 import errors from "./errors.reducer";
 import user from "./user.reducer";
-import discount from "./discount.reducer";
+import discounts from "./discount.reducer";
 
 // rootReducer is the primary reducer for our entire project
 // It bundles up all of the other reducers so our project can use them.
