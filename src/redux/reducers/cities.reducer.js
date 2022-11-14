32f00a2d7d8da1a1c8e24cb53ce577ab66d83e76
David Lindberg <david.wesley.lindberg@gmail.com>
2022-11-14 11:57:22 -0600

    created sagas for getting all cities and getting cities closest to user.

--- src/redux/reducers/cities.reducer.js
+++ src/redux/reducers/cities.reducer.js
@@ -2,25 +2,25 @@ import { combineReducers } from "redux";
 
 // reducer to store all cities.
 const citiesReducer = (state = [], action) => {
-    switch (action.type) {
-        case 'SET_ALL_CITIES':
-            return action.payload;
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case "SET_ALL_CITIES":
+      return action.payload;
+    default:
+      return state;
+  }
+};
 
 // reducer to store closest cities to user.
 const closeCitiesReducer = (state = [], action) => {
-    switch (action.type) {
-        case 'SET_CLOSE_CITIES':
-            return action.payload;    
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case "SET_CLOSE_CITIES":
+      return action.payload;
+    default:
+      return state;
+  }
+};
 
 export default combineReducers({
-    citiesReducer,
-    closeCitiesReducer,
+  citiesReducer,
+  closeCitiesReducer,
 });
--- src/redux/sagas/cities.saga.js
+++ src/redux/sagas/cities.saga.js
@@ -1,8 +1,41 @@
+import { takeEvery, put } from "redux-saga/effects";
+const axios = require("axios");
 
+// route: /api/cities
 
 // saga to direct actions to the correct sagas.
 function* citiesSaga() {
-
+  yield takeEvery("GET_ALL_CITIES", getAllCities);
+  yield takeEvery("GET_CLOSE_CITIES", getCloseCities);
 }
 
+// saga to get all cities and store them in redux.
+function* getAllCities() {
+  try {
+    // fetch all cities from the database.
+    const response = yield axios.get("/api/cities");
+    // store the cities into redux.
+    yield put({ type: "SET_ALL_CITIES", payload: response.data });
+  } catch (err) {
+    console.log("Error in getting all cities", err);
+  }
+}
 
+// saga to get cities closest to user.
+function* getCloseCities(action) {
+  try {
+    // shorten the action.payload by assigning it to coords.
+    const coords = action.payload;
+    // console.log(coords);
+    // fetch the closest cities.
+    const response = yield axios.get(
+      `/api/cities/close?lat=${coords.lat}&lng=${coords.lng}`
+    );
+    // store the closest cities in redux.
+    yield put({ type: "SET_LOCATIONS", payload: response.data });
+  } catch (err) {
+    console.log("Error in getting cities closest to user", err);
+  }
+}
+
+export default citiesSaga;
