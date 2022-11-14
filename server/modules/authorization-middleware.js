e4f4a45273282caeb3272f244413d87b4f790783
Lloyd Prock <lloyd.prock@gmail.com>
2022-11-14 14:55:03 -0600

    working middleware authorization and updated accounts router

--- server/modules/authorization-middleware.js
+++ server/modules/authorization-middleware.js
@@ -3,33 +3,48 @@ const pool = require("./pool");
 const rejectUnauthorizedUser = async (req, res, next) => {
   // Declare initial setup values
   let isUserAuthorized = false;
-  const currentUserInfo = {};
-  const userQueryText = `SELECT * from "user" where "id" = $1;`;
+  let currentUserInfo = {};
+  const userQueryText = `SELECT
+    id,
+    username,
+    first_name,
+    last_name,
+    email,
+    primary_member_id,
+    is_authorized,
+    is_verified,
+    review_pending,
+    dues_paid,
+    membership_number,
+    admin_level
+    FROM "user" where "id" = $1;`;
 
   // Begin SQL pull for current user
   await pool
     .query(userQueryText, [req.user.id])
     .then((result) => {
       // Save current user to a global variable
-      currentUserInfo = result.rows;
+      currentUserInfo = result.rows[0];
     })
     .catch((error) => {
       console.log("error caught in rejectUnauthorized firstQuery :>> ", error);
     });
 
   // Check if user has a membership number (if they do they are a Shriner, if they don't they are a dependent)
-  if (!currentUserInfo.membership_number) {
+  if (currentUserInfo.membership_number) {
     // If user is a Shriner then set their authorization to their current database authorization
     isUserAuthorized = currentUserInfo.is_authorized;
   } else if (
-    currentUserInfo.membership_number &&
+    !currentUserInfo.membership_number &&
     currentUserInfo.is_authorized
   ) {
     // If a dependent and currently authorized, check if Shriner member is also authorized
     await pool
       .query(userQueryText, [currentUserInfo.primary_member_id])
       .then((result) => {
-        isUserAuthorized = result.rows.is_authorized;
+        if (result.rows[0] && result.rows[0].is_authorized) {
+          isUserAuthorized = result.rows[0].is_authorized;
+        }
       })
       .catch((error) => {
         console.log(
