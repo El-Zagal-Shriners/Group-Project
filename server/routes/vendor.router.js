8b0a40754ce6494a78120ddce68970b60ee6cfd3
Lloyd Prock <lloyd.prock@gmail.com>
2022-11-14 12:08:03 -0600

    added authentication to router

--- server/routes/vendor.router.js
+++ server/routes/vendor.router.js
@@ -1,16 +1,19 @@
 const express = require("express");
+const {
+  rejectUnauthenticated,
+} = require("../modules/authentication-middleware");
 const pool = require("../modules/pool");
 const router = express.Router();
 
 // Setup routes for /api/vendors
 
 // Begin GET route for all vendors
-router.get("/", (req, res) => {
+router.get("/", rejectUnauthenticated, (req, res) => {
   // GET route code here
 }); // End GET for all vendors
 
 // Begin POST for vendors
-router.post("/", (req, res) => {
+router.post("/", rejectUnauthenticated, (req, res) => {
   // POST route code here
 }); // End POST for vendors
 
