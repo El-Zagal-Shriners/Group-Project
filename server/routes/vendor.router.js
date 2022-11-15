2cb1713d7493fdbbf4949ab269fc05ae7f711c89
David Vetter <ddvetter23@gmail.com>
2022-11-15 13:55:28 -0600

    added PUT for editing a vendor to vendor router

--- server/routes/vendor.router.js
+++ server/routes/vendor.router.js
@@ -43,4 +43,30 @@ router.post("/", rejectUnauthenticated, (req, res) => {
   
 }); // End POST for new vendors
 
+// PUT for editing existing vendor info by vendorId
+router.put('/', rejectUnauthenticated, (req, res) => {
+  // Variables from req.body
+  const name = req.body.name;
+  const address = req.body.address;
+  const city = req.body.city;
+  const stateCode = req.body.stateCode;
+  const zip = req.body.zip;
+  const vendorId = req.body.vendorId;
+  // SQL for PUT
+  const query = `UPDATE "vendors" 
+                 SET "name"=$1, "address"=$2, "city"=$3, "state_code"=$4, "zip"=$5 
+                 WHERE "id"=$6;`;
+  // Run SQL against database
+  pool.query(query, [name, address, city, stateCode, zip, vendorId])
+      .then(result => {
+        // Send success status  
+        res.sendStatus(200);
+      })
+      .catch(err => {
+          // Log error and send error status if error occurs
+          console.log('Error editing vendor ', err);
+          res.sendStatus(500);
+      });
+}); // End vendor editing PUT
+
 module.exports = router;
