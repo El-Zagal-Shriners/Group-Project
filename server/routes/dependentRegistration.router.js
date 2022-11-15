ca5f10aca20ab8e2b3a337e9bb3cd96b6f50f974
Lee Baesler <lee.baesler@gmail.com>
2022-11-15 09:57:04 -0600

    working on saga, route, and dependent registration page

--- server/routes/dependentRegistration.router.js
+++ server/routes/dependentRegistration.router.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const pool = require('../modules/pool')
+const express = require("express");
+const pool = require("../modules/pool");
 const router = express.Router();
 
 // router to post to "user" table first_name, last_name, email, username, and password columns
-router.post('/', (req,res) => {
-    let dependent = req.body
-})
+router.post("/", (req, res) => {
+  let dependent = req.body;
+});
