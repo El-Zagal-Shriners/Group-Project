3e9e7aecd7937e19db0710aa498f87cfd59cc2bb
David Vetter <ddvetter23@gmail.com>
2022-11-15 11:37:52 -0600

    added GET route for all vendors

--- server/routes/vendor.router.js
+++ server/routes/vendor.router.js
@@ -10,6 +10,15 @@ const router = express.Router();
 // Begin GET route for all vendors
 router.get("/", rejectUnauthenticated, (req, res) => {
   // GET route code here
+  const query = `SELECT * FROM "vendors"`;
+  pool.query(query)
+      .then(result => {
+        res.send(result.rows);
+      })
+      .catch((err) => {
+        console.log('Error in getting vendors: ', err);
+        res.sendStatus(500);
+      })
 }); // End GET for all vendors
 
 // Begin POST for vendors
