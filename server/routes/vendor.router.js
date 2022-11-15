b2f795ab544b86d2c3e5ade66302b2705efe6f18
David Vetter <ddvetter23@gmail.com>
2022-11-15 12:07:04 -0600

    added Post for new vendor

--- server/routes/vendor.router.js
+++ server/routes/vendor.router.js
@@ -21,9 +21,26 @@ router.get("/", rejectUnauthenticated, (req, res) => {
       })
 }); // End GET for all vendors
 
-// Begin POST for vendors
+// Begin POST for new vendors
 router.post("/", rejectUnauthenticated, (req, res) => {
-  // POST route code here
-}); // End POST for vendors
+  const name = req.body.name;
+  const address = req.body.address;
+  const city = req.body.city;
+  const stateCode = req.body.stateCode;
+  const zip = req.body.zip;
+  const query = `INSERT INTO "vendors" ("name", "address", "city", "state_code", "zip")
+                 VALUES ($1, $2, $3, $4, $5);`;
+  pool.query(query, [name, address, city, stateCode, zip])
+      .then(result => {
+        // send success status
+          res.sendStatus(201);
+      })
+      .catch((err) => {
+        // log error and send error status if error occurs
+          console.log('Error in POST new vendor: ', err);
+          res.sendStatus(500);
+      })
+  
+}); // End POST for new vendors
 
 module.exports = router;
