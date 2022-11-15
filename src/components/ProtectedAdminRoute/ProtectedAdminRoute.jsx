6cbfc3e68bff62d64c2fac8b2a1ee146893bfd04
Lloyd Prock <lloyd.prock@gmail.com>
2022-11-15 13:52:46 -0600

    Setup initial admin protected route

---
+++ src/components/ProtectedAdminRoute/ProtectedAdminRoute.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Route } from "react-router-dom";
+import { useSelector } from "react-redux";
+import DiscountsPage from "../DiscountsPage/DiscountsPage";
+
+// A Custom Wrapper Component -- This will keep our code DRY.
+// Responsible for watching redux state, and returning an appropriate component
+// API for this component is the same as a regular route
+
+// THIS IS NOT SECURITY! That must be done on the server
+// This simply stops a user from being able to add the admin landing page
+// Admin functions and server routes must be protected by admin middleware
+
+function ProtectedAdminRoute({ component, children, ...props }) {
+  const user = useSelector((store) => store.user);
+
+  // Get component from props
+  const ProtectedComponent = component || (() => children);
+
+  return (
+    <Route
+      // Pass props to this route component
+      {...props}
+    >
+      {user.id && user.admin_level > 0 ? (
+        // If the user is logged in and has an administrative level, show the protected component
+        <ProtectedComponent />
+      ) : (
+        // Otherwise, redirect to the Discounts page
+        <DiscountsPage />
+      )}
+    </Route>
+  );
+}
+
+export default ProtectedAdminRoute;
