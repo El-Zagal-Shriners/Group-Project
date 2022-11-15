95aa2ba8066b8a2801e2a3b862f76e9b95869404
David Vetter <ddvetter23@gmail.com>
2022-11-15 07:50:05 -0600

    added saga for new discount POST

--- src/redux/sagas/discount.saga.js
+++ src/redux/sagas/discount.saga.js
@@ -1,6 +1,7 @@
-import { put, take, takeLatest } from "redux-saga/effects";
+import { put, takeLatest } from "redux-saga/effects";
 import axios from "axios";
 
+// GET all discounts will calculated usage stats from database
 function* getDiscounts() {
   try {
     console.log("In discounts saga");
@@ -14,8 +15,20 @@ function* getDiscounts() {
   }
 }
 
+// POST a new discount to the discount table in database
+function* addDiscount(action) {
+  try {
+    console.log("In add discount");
+    yield axios.post(`api/discount/`, action.payload);
+    yield put({ type: "GET_DISCOUNTS" });
+  } catch (err) {
+    console.log("Error adding discount: ", err);
+  }
+}
+
 function* discountSaga() {
   yield takeLatest("GET_DISCOUNTS", getDiscounts);
+  yield takeLatest("ADD_DISCOUNT", addDiscount);
 }
 
 export default discountSaga;
