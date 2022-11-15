fa1d7151ec4ebe0d38fdb926883127140e622713
David Vetter <ddvetter23@gmail.com>
2022-11-15 15:13:53 -0600

    added POST saga for adding a new vendor

--- src/redux/sagas/vendor.saga.js
+++ src/redux/sagas/vendor.saga.js
@@ -1,14 +1,15 @@
 import axios from "axios";
 import { put, takeEvery } from "redux-saga/effects";
 
+// Setup config for credentials
+const config = {
+  headers: { "Content-Type": "application/json" },
+  withCredentials: true,
+};
+
 // worker Saga: will be fired on "FETCH_USER" actions
 function* fetchVendors() {
   try {
-    // Setup config for credentials
-    const config = {
-      headers: { "Content-Type": "application/json" },
-      withCredentials: true,
-    };
     // Store response from axios
     const response = yield axios.get("/api/vendors", config);
     // Send response to reducer
@@ -18,10 +19,21 @@ function* fetchVendors() {
   }
 } // End fetchVendors
 
+// POST new vendor on ADD_VENDOR
+function* addVendor(action) {
+  try{
+      yield axios.post(`/api/vendors`, action.payload, config);
+      yield put({ type: "FETCH_VENDORS" });
+  } catch (error){
+      console.log('Error in adding vendor', error);
+  }
+}
+
 // worker Saga will fire on "FETCH_VENDORS"
 
 function* vendorSaga() {
   yield takeEvery("FETCH_VENDORS", fetchVendors);
+  yield takeEvery("ADD_VENDOR", addVendor);
 } // End vendorSaga
 
 export default vendorSaga;
