80c21ee3536a585ac7afc7b539cbc81c14b2dceb
Lee Baesler <lee.baesler@gmail.com>
2022-11-16 10:07:27 -0600

    working on floating label

--- src/components/DependentRegistrationPage/DependentRegistration.jsx
+++ src/components/DependentRegistrationPage/DependentRegistration.jsx
@@ -1,6 +1,8 @@
 import React, { useRef, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import "./DependentRegistrationPage.css";
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Form from 'react-bootstrap/Form';
 import button from "react-bootstrap/Button";
 import { useParams } from "react-router-dom";
 import UpdatedNavBar from "../Nav/Nav";
