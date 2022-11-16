5fe7b182f193778a734b2758ea8015ed206a1cc5
Lee Baesler <lee.baesler@gmail.com>
2022-11-16 10:12:29 -0600

    switching to branch for floating label

--- src/components/DependentRegistrationPage/DependentRegistration.jsx
+++ src/components/DependentRegistrationPage/DependentRegistration.jsx
@@ -1,8 +1,6 @@
 import React, { useRef, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import "./DependentRegistrationPage.css";
-import FloatingLabel from 'react-bootstrap/FloatingLabel';
-import Form from 'react-bootstrap/Form';
 import button from "react-bootstrap/Button";
 import { useParams } from "react-router-dom";
 import UpdatedNavBar from "../Nav/Nav";
@@ -37,55 +35,57 @@ function DependentRegistrationPage() {
     <>
       <UpdatedNavBar />
       <div className="background">
-      <div class="container text-center">
-        <h2 className="header">Dependent Registration Page</h2>
-      </div>
-      
-      <form onSubmit={createAccount} className="createAccount">
         <div class="container text-center">
-          <input
-            required
-            placeholder="First Name"
-            value={firstName}
-            onChange={(event) => setFirstName(event.target.value)}
-          />
+          <h2 className="header">Dependent Registration Page</h2>
         </div>
-        <div class="container text-center">
-          <input
-            required
-            placeholder="Last Name"
-            value={lastName}
-            onChange={(event) => setLastName(event.target.value)}
-          />
-        </div>
-        <div class="container text-center">
-          <input
-            required
-            placeholder="Email"
-            value={email}
-            onChange={(event) => setEmail(event.target.value)}
-          />
-        </div>
-        <div class="container text-center">
-          <input
-            required
-            placeholder="Username"
-            value={usernameIn}
-            onChange={(event) => setUsernameIn(event.target.value)}
-          />
-        </div>
-        <div class="container text-center">
-          <input
-            required
-            placeholder="Password"
-            value={passwordIn}
-            onChange={(event) => setPasswordIn(event.target.value)}
-          />
-        </div>
-        <div class="container text-center">
-          <button type="submit" class="btn btn-primary" >Create Account</button>
-        </div>
-      </form>
+
+        <form onSubmit={createAccount} className="createAccount">
+          <div class="container text-center">
+            <input
+              required
+              placeholder="First Name"
+              value={firstName}
+              onChange={(event) => setFirstName(event.target.value)}
+            />
+          </div>
+          <div class="container text-center">
+            <input
+              required
+              placeholder="Last Name"
+              value={lastName}
+              onChange={(event) => setLastName(event.target.value)}
+            />
+          </div>
+          <div class="container text-center">
+            <input
+              required
+              placeholder="Email"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+            />
+          </div>
+          <div class="container text-center">
+            <input
+              required
+              placeholder="Username"
+              value={usernameIn}
+              onChange={(event) => setUsernameIn(event.target.value)}
+            />
+          </div>
+          <div class="container text-center">
+            <input
+              required
+              placeholder="Password"
+              value={passwordIn}
+              onChange={(event) => setPasswordIn(event.target.value)}
+            />
+          </div>
+          <div class="container text-center">
+            <button type="submit" class="btn btn-primary">
+              Create Account
+            </button>
+          </div>
+        </form>
       </div>
     </>
   );
