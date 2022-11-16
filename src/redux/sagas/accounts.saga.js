59024aa1b9ae384b2597a76007f711718629c57b
David Vetter <ddvetter23@gmail.com>
2022-11-16 13:00:15 -0600

    added saga for getting dependents for current user

--- src/redux/sagas/accounts.saga.js
+++ src/redux/sagas/accounts.saga.js
@@ -1,12 +1,13 @@
 import { put, takeEvery } from "redux-saga/effects";
 import axios from "axios";
 
+const config = {
+  headers: { "Content-Type": "application/json" },
+  withCredentials: true,
+};
+
 function* fetchAllAccounts() {
   try {
-    const config = {
-      headers: { "Content-Type": "application/json" },
-      withCredentials: true,
-    };
     const accounts = yield axios.get("/api/accounts", config);
     console.log("GET accounts", accounts.data);
     yield put({ type: "FETCH_ACCOUNTS", payload: accounts.data });
@@ -15,8 +16,22 @@ function* fetchAllAccounts() {
   }
 }
 
+// SAGA to get all dependent accounts for the current user
+function* getDependents() {
+  try {
+  const dependents = yield axios.get(`api/accounts/dependants`, config);
+  yield put({
+      type: "SET_DEPENDANTS",
+      payload: dependents.data
+  });
+  } catch (err) {
+      console.log('Error getting dependents', err);
+  }
+}
+
 function* accountsSaga() {
   yield takeEvery("GET_ACCOUNTS", fetchAllAccounts);
+  yield takeEvery("GET_DEPENDENTS", getDependents)
 }
 
 export default accountsSaga;
