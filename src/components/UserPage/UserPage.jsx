636c7dc9ab3a5436a287d9d01d5b3dc0cf924311
David Vetter <ddvetter23@gmail.com>
2022-11-16 12:12:47 -0600

    added user info to profile page

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import LogOutButton from "../LogOutButton/LogOutButton";
 import { useSelector } from "react-redux";
 import UpdatedNavBar from "../Nav/Nav";
+import NavbarCollapse from "react-bootstrap/esm/NavbarCollapse";
 
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
@@ -10,9 +11,11 @@ function UserPage() {
     <>
       <UpdatedNavBar />
       <div className="container">
-        <h2>Welcome, {user.username}!</h2>
-        <p>Your ID is: {user.id}</p>
-        <LogOutButton className="btn" />
+        <h2>Welcome, {user.first_name} {user.last_name}!</h2>
+        <p>Username: {user.username}</p>
+        <p>Email: {user.email}</p>
+        <p>Member Number: {user.membership_number}</p>
+        <LogOutButton className="btn btn-outline-danger" />
       </div>
     </>
   );
