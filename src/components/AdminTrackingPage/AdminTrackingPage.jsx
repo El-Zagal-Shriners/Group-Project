66996409757a77a1120f91288e28f09b57f9549b
Lloyd Prock <lloyd.prock@gmail.com>
2022-11-16 16:23:50 -0600

    working dropdown

--- src/components/AdminTrackingPage/AdminTrackingPage.jsx
+++ src/components/AdminTrackingPage/AdminTrackingPage.jsx
@@ -1,10 +1,60 @@
 import UpdatedNavBar from "../Nav/Nav";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 
 function AdminTrackingPage() {
+  const dispatch = useDispatch();
+  const allVendors = useSelector((store) => store.vendors);
+  const [currentSelected, setCurrentSelected] = useState(0);
+
+  function handleSelect(event) {
+    setCurrentSelected(event);
+  }
+
+  useEffect(() => {
+    dispatch({ type: "FETCH_VENDORS" });
+  }, []);
+
   return (
     <>
       <UpdatedNavBar />
-      <h2>Admin Tracking Page</h2>
+      <div className="container d-flex flex-column justify-content-center align-items-center">
+        <DropdownButton
+          as={ButtonGroup}
+          key="primary"
+          id={`discountDropdown`}
+          variant="primary"
+          title="Select Vendor"
+          className="w-75"
+          onSelect={handleSelect}
+        >
+          <Dropdown.Item
+            eventKey="default"
+            active={currentSelected === "default" && true}
+          >
+            All
+          </Dropdown.Item>
+          {allVendors.map((vendor) => {
+            return (
+              <Dropdown.Item
+                key={vendor.id}
+                eventKey={vendor.id}
+                active={Number(currentSelected) === Number(vendor.id) && true}
+              >
+                {vendor.name}
+              </Dropdown.Item>
+            );
+          })}
+        </DropdownButton>
+
+        <div>
+          <p>Stuff</p>
+        </div>
+      </div>
     </>
   );
 }
