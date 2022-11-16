4c1e4bf8a8b62dcdceb6fb5b0ecf14c072925b3e
David Vetter <ddvetter23@gmail.com>
2022-11-16 15:18:48 -0600

    added display of dependents to user profile, display basic user information

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,21 +1,43 @@
-import React from "react";
+import { useEffect } from 'react';
 import LogOutButton from "../LogOutButton/LogOutButton";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import UpdatedNavBar from "../Nav/Nav";
 import NavbarCollapse from "react-bootstrap/esm/NavbarCollapse";
+
 
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
+  const accounts = useSelector((store) => store.accounts);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch({
+      type: 'GET_DEPENDENTS'
+    });
+  }, []);
+
   return (
     <>
       <UpdatedNavBar />
       <div className="container">
         <h2>Welcome, {user.first_name} {user.last_name}!</h2>
-        <p>Username: {user.username}</p>
-        <p>Email: {user.email}</p>
-        <p>Member Number: {user.membership_number}</p>
-        <LogOutButton className="btn btn-outline-danger" />
+        <p>Username: {user.username}<br />
+          Email: {user.email}<br />
+          Member Number: {user.membership_number}
+        </p>
+        <button className="btn btn-contained-info">Edit</button>
+        {accounts.accountDependents.length > 0 && 
+          <>
+          <h2>Dependent Accounts</h2>
+            {accounts.accountDependents.map((dependent)=>
+            <>
+              <p>Name: {dependent.first_name} {dependent.last_name} <br />Username: {dependent.username}<br /> Email: {dependent.email}</p>
+              <button className="btn btn-outline-danger">Remove</button><br/><br />
+            </>
+            )}
+          </>}
+          <LogOutButton className="btn btn-outline-warning" />
       </div>
     </>
   );
