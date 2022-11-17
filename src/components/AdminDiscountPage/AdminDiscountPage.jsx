aee59c51e4a13cab434d2fe43b5cf226323d3c96
Lee Baesler <lee.baesler@gmail.com>
2022-11-17 13:55:01 -0600

    working on admit discount page

--- src/components/AdminDiscountPage/AdminDiscountPage.jsx
+++ src/components/AdminDiscountPage/AdminDiscountPage.jsx
@@ -12,27 +12,35 @@ function AdminDiscountPage() {
   );
 
   console.log("discounts", discounts);
-  console.log('vendors', vendors)
+  console.log("vendors", vendors);
 
   useEffect(() => {
     dispatch({
-      type: "GET_ADMIN_DISCOUNTS"
+      type: "GET_ADMIN_DISCOUNTS",
     });
     console.log("discounts", discounts);
   }, []);
 
   useEffect(() => {
     dispatch({
-      type: "GET_VENDORS"
+      type: "FETCH_VENDORS",
     });
-  }, [])
+    console.log("vendor", vendors);
+  }, []);
 
   return (
     <>
       <UpdatedNavBar />
-      <h2>Admin Discount Page</h2> <h2> Discount Description</h2>
-      
+      <h2>Admin Discount Page</h2>
+      <div className="text-primary">Vendors {vendors.map(vendor => (
+                        <div className="text-primary">{vendor.name}, {vendor.city}</div>
+                    ))}</div>
+      <div className="text-primary">Discounts {discounts.map(discount => (
+                        <div className="text-primary">{discount.description}</div>
+                        
+                    ))}</div>
     </>
+  
   );
 }
 
