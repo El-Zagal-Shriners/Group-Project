2bbc744d11baa969e5f4ce9f5b3eed42d8b39aae
Joey Pettit <pettitbass@gmail.com>
2022-11-17 09:52:05 -0600

    pulling from master

--- src/components/DiscountsPage/DiscountsPage.jsx
+++ src/components/DiscountsPage/DiscountsPage.jsx
@@ -29,54 +29,39 @@ function DiscountsPage() {
 
   const [showFilterOffCanvas, setShowFilterOffCanvas] = useState(false);
 
-  function filterDiscounts() {
-    let discountList = [...allMemberDiscounts];
-    console.log('in filter')
+  
 
 
-    // ~~~THIS IS A START TO USING INDEXOF TO FILTER DISCOUNT ARRAY
-    // const categoryIds = selectedCategories.map((thisCat) => thisCat.id);
-    // const cityNames = selectedCities.map((thisCity) => thisCity.city);
-    // if(categoryIds.length !== 0 || cityNames.length !== 0){
-    // discountList = discountList.filter((thisDiscount)=>{
-    //   console.log('in filter', categoryIds.indexOf(thisDiscount.category_id))
-    //   if(categoryIds.indexOf(thisDiscount.category_id) === -1){
-    //     console.log('in cat indexOf, discount it', thisDiscount.category_id);
-    //     return false;
-    //   } else if(cityNames.indexOf(thisDiscount.city) === -1){
-    //     console.log('in city indexOf, discount it', thisDiscount);
-    //     return false;
-    //   } else {
-    //     return true;
-    //   }
-    // })}
+  // function filterByCity() {
+  //   let discountList = [...allMemberDiscounts];
 
-    // for each selected category, filter out discounts that don't match
-    for (let category of selectedCategories) {
-      discountList = discountList.filter((discount) => {
-        return discount.category_id === category.id;
-      });
-    }
+    
+  //   }
 
-    for (let city of selectedCities) {
-      discountList = discountList.filter((discount) => {
-        return discount.city === city.city;
-      });
-    }
+  //   function filterByCategory(){
+  //      // for each selected category, filter out discounts that don't match
+  //     for (let category of selectedCategories) {
+  //     discountList = discountList.filter((discount) => {
+  //       return discount.category_id === category.id;
+  //     });
+  //   }
 
-    dispatch({ type: "SET_FILTERED_DISCOUNTS", payload: discountList });
-  }
+        // dispatch({ type: "SET_FILTERED_DISCOUNTS", payload: discountList });
+  //   }
 
+    
+  
+  
   useEffect(() => dispatch({ type: "GET_MEMBER_DISCOUNTS" }), []);
   useEffect(
     () => console.log("memberDiscounts are", allMemberDiscounts),
     [allMemberDiscounts]
   );
-  // updated filtered discount array when changes made to filter
-  useEffect(
-    () => filterDiscounts(),
-    [selectedCities, selectedCategories, allMemberDiscounts]
-  );
+  // // updated filtered discount array when changes made to filter
+  // useEffect(
+  //   () => filterDiscounts(),
+  //   [selectedCities, selectedCategories, allMemberDiscounts]
+  // );
 
   return (
     <>
