e549a306600bd898ac5cda4ce800aaed6d1edd30
Lee Baesler <lee.baesler@gmail.com>
2022-11-17 12:04:11 -0600

    working on admin discount page

--- src/components/AdminDiscountPage/AdminDiscountPage.jsx
+++ src/components/AdminDiscountPage/AdminDiscountPage.jsx
@@ -1,10 +1,37 @@
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
 import UpdatedNavBar from "../Nav/Nav";
 
 function AdminDiscountPage() {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const vendors = useSelector((store) => store.vendors);
+  const discounts = useSelector(
+    (store) => store.discounts.adminDiscountsReducer
+  );
+
+  console.log("discounts", discounts);
+  console.log('vendors', vendors)
+
+  useEffect(() => {
+    dispatch({
+      type: "GET_ADMIN_DISCOUNTS"
+    });
+    console.log("discounts", discounts);
+  }, []);
+
+  useEffect(() => {
+    dispatch({
+      type: "GET_VENDORS"
+    });
+  }, [])
+
   return (
     <>
       <UpdatedNavBar />
-      <h2>Admin Discount Page</h2>{" "}
+      <h2>Admin Discount Page</h2> <h2> Discount Description</h2>
+      
     </>
   );
 }
