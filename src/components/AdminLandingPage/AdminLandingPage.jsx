b48965c4416274c3dcc120aa317fd2afc3f93897
Lee Baesler <lee.baesler@gmail.com>
2022-11-16 22:21:41 -0600

    admin landing page with links to manage members, update discounts, and track discounts

--- src/components/AdminLandingPage/AdminLandingPage.jsx
+++ src/components/AdminLandingPage/AdminLandingPage.jsx
@@ -15,9 +15,11 @@ function AdminLandingPage() {
   }
 
   function discountTracker() {
-    history.push("/admintracking")
+    history.push("/admintracking");
   }
 
+  // three bootstrap cards that direct the adminstrator to make changes
+  //to membership information, discounts, and track discounts
   return (
     <>
       <UpdatedNavBar />
@@ -25,62 +27,60 @@ function AdminLandingPage() {
         Admin Landing Page
       </h2>{" "}
       <br />
-        <div class="card-body">
+      <div class="card-body">
         <div class="container text-center">
-          <h5 class="card-title text-primary">Manage Shriner Members</h5> <br/>
+          <h5 class="card-title text-primary">Manage Shriner Members</h5> <br />
           <p class="card-text">
             Administrative access to member's portal information. Administrator
             can update, edit, remove or delete member and dependent portal
             access.
           </p>
           <Button
-          size="lg"
-          variant="outline-primary"
-          onClick={manageMembers}
-          className="me-2 d-flex justify-content-center container text-center"
-        >
-          Manage Members
+            size="lg"
+            variant="outline-primary"
+            onClick={manageMembers}
+            className="me-2 d-flex justify-content-center container text-center"
+          >
+            Manage Members
           </Button>
         </div>
       </div>
       <br />
       <div class="container text-center">
         <div class="card-body">
-          <h5 class="card-title text-primary">Discount Manager</h5> <br/>
+          <h5 class="card-title text-primary">Discount Manager</h5> <br />
           <p class="card-text">
             Administrative access to discount portal information. Administrator
             can update, edit, remove or delete company and discount information.
           </p>
           <Button
-          size="lg"
-          variant="outline-primary"
-          onClick={manageDiscounts}
-          className="me-2 d-flex justify-content-center container text-center"
-        >
-          Manage Discounts
+            size="lg"
+            variant="outline-primary"
+            onClick={manageDiscounts}
+            className="me-2 d-flex justify-content-center container text-center"
+          >
+            Manage Discounts
           </Button>
         </div>
       </div>
       <br />
       <div class="container text-center">
         <div class="card-body">
-          <h5 class="card-title text-primary">Discount Tracker</h5> <br/>
+          <h5 class="card-title text-primary">Discount Tracker</h5> <br />
           <p class="card-text">
-            Administrative access to discount tracker. Administrator can view, reset, and delete discount tracker.
+            Administrative access to discount tracker. Administrator can view,
+            reset, and delete discount tracker.
           </p>
           <Button
-          size="lg"
-          variant="outline-primary"
-          onClick={discountTracker}
-          className="me-2 d-flex justify-content-center container text-center"
-        >
-          Track Discounts
+            size="lg"
+            variant="outline-primary"
+            onClick={discountTracker}
+            className="me-2 d-flex justify-content-center container text-center"
+          >
+            Track Discounts
           </Button>
         </div>
       </div>
-      {/* <h2> Manage Members</h2>
-      <h2> Manage Discounts</h2>
-      <h2>Discount Tracker</h2> */}
     </>
   );
 }
