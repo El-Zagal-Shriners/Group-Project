a9be8b9213bb0308ce4ccd5f74e22c2d671846be
Lee Baesler <lee.baesler@gmail.com>
2022-11-19 14:49:57 -0600

    woring on add discont page

---
+++ src/components/AdminDiscountPage/AddDiscount.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import FloatingLabel from "react-bootstrap/FloatingLabel";
+import Form from "react-bootstrap/Form";
+import { useParams } from "react-router-dom";
+import UpdatedNavBar from "../Nav/Nav";
+
+function AddDiscount() {
+    
+    return (
+        <UpdatedNavBar />
+    )
+}
+
+export default AddDiscount;
