988de89752651eb2e0cbccfe1cc2c3c52f8992a4
David Lindberg <david.wesley.lindberg@gmail.com>
2022-11-21 11:26:02 -0600

    converting to flexbox for dynamic height of tables

--- src/components/AdminMembershipPage/AccountTables/ApprovalTable/ApprovalTable.jsx
+++ src/components/AdminMembershipPage/AccountTables/ApprovalTable/ApprovalTable.jsx
@@ -6,29 +6,33 @@ function ApprovalTable({ approvals }) {
   return (
     <div className="w-100">
       <p>New Members</p>
-      <ListGroup>
-        <ListGroup.Item className="p-1">
-          <ListGroup horizontal>
-            <ListGroup.Item className="col-4 p-0 text-center">
-              First Name
-            </ListGroup.Item>
-            <ListGroup.Item className="col-4 p-0 text-center">
-              Last Name
-            </ListGroup.Item>
-            <ListGroup.Item className="col p-0 text-center">
-              Member Number
-            </ListGroup.Item>
-          </ListGroup>
-        </ListGroup.Item>
-        <div
-          className="member-table"
-          style={{ overflowY: "scroll", height: "15vh" }}
-        >
-          {approvals.map((approval) => (
-            <ApprovalItem key={approval.id} approval={approval} />
-          ))}
-        </div>
-      </ListGroup>
+      {approvals.length > 0 ? (
+        <ListGroup>
+          <ListGroup.Item className="p-1">
+            <ListGroup horizontal>
+              <ListGroup.Item className="col-4 p-0 text-center">
+                First Name
+              </ListGroup.Item>
+              <ListGroup.Item className="col-4 p-0 text-center">
+                Last Name
+              </ListGroup.Item>
+              <ListGroup.Item className="col p-0 text-center">
+                Member Number
+              </ListGroup.Item>
+            </ListGroup>
+          </ListGroup.Item>
+          <div
+            className="member-table"
+            style={{ overflowY: "scroll", maxHeight: "15vh" }}
+          >
+            {approvals.map((approval) => (
+              <ApprovalItem key={approval.id} approval={approval} />
+            ))}
+          </div>
+        </ListGroup>
+      ) : (
+        <div>No new members</div>
+      )}
     </div>
   );
 }
--- src/components/AdminMembershipPage/AccountTables/ReviewTable/ReviewTable.jsx
+++ src/components/AdminMembershipPage/AccountTables/ReviewTable/ReviewTable.jsx
@@ -6,29 +6,33 @@ function ReviewTable({ reviews }) {
   return (
     <div className="vw-100">
       <p>Pending Review</p>
-      <ListGroup>
-        <ListGroup.Item className="p-1">
-          <ListGroup horizontal>
-            <ListGroup.Item className="col-4 p-0 text-center">
-              First Name
-            </ListGroup.Item>
-            <ListGroup.Item className="col-4 p-0 text-center">
-              Last Name
-            </ListGroup.Item>
-            <ListGroup.Item className="col p-0 text-center">
-              Member Number
-            </ListGroup.Item>
-          </ListGroup>
-        </ListGroup.Item>
-        <div
-          className="member-table"
-          style={{ overflowY: "scroll", height: "15vh" }}
-        >
-          {reviews.map((review) => (
-            <ReviewItem key={review.id} review={review} />
-          ))}
-        </div>
-      </ListGroup>
+      {reviews.length > 0 ? (
+        <ListGroup>
+          <ListGroup.Item className="p-1">
+            <ListGroup horizontal>
+              <ListGroup.Item className="col-4 p-0 text-center">
+                First Name
+              </ListGroup.Item>
+              <ListGroup.Item className="col-4 p-0 text-center">
+                Last Name
+              </ListGroup.Item>
+              <ListGroup.Item className="col p-0 text-center">
+                Member Number
+              </ListGroup.Item>
+            </ListGroup>
+          </ListGroup.Item>
+          <div
+            className="member-table"
+            style={{ overflowY: "scroll", maxHeight: "15vh" }}
+          >
+            {reviews.map((review) => (
+              <ReviewItem key={review.id} review={review} />
+            ))}
+          </div>
+        </ListGroup>
+      ) : (
+        <div>No pending reviews</div>
+      )}
     </div>
   );
 }
