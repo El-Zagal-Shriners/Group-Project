dd4d010b005dc851453d2bed38e3e892166edb0c
Lee Baesler <lee.baesler@gmail.com>
2022-11-21 11:59:17 -0600

    working on admin discount page

--- src/components/AdminDiscountPage/AdminDiscountPage.jsx
+++ src/components/AdminDiscountPage/AdminDiscountPage.jsx
@@ -8,6 +8,8 @@ import DropdownButton from "react-bootstrap/DropdownButton";
 import Card from "react-bootstrap/Card";
 import DiscountModal from "./DiscountModal";
 import DiscountItem from "./DiscountItem";
+import Button from "react-bootstrap/Button";
+
 
 function AdminDiscountPage() {
   const dispatch = useDispatch();
@@ -16,6 +18,14 @@ function AdminDiscountPage() {
   const discounts = useSelector(
     (store) => store.discounts.adminDiscountsReducer
   );
+
+  function addVendor() {
+    history.push("/adminaddvendor");
+  }
+
+  function addDiscount() {
+    history.push("/adminadddiscount");
+  }
 
   let filteredDiscounts = [...discounts];
   const [currentSelected, setCurrentSelected] = useState("default");
@@ -43,6 +53,38 @@ function AdminDiscountPage() {
     <>
       <UpdatedNavBar />
       <div className="container d-flex flex-column justify-content-center align-items-center">
+      <div className="container text-center">
+        <div className="card-body">
+          <h5 className="card-title text-primary">Add Vendor</h5> <br />
+          <p className="card-text">
+            Administrative access to add vendor information.
+          </p>
+          <Button
+            size="lg"
+            variant="outline-primary"
+            onClick={addVendor}
+            className="me-2 d-flex justify-content-center container text-center"
+          >
+            Add Vendor
+          </Button>
+        </div>
+      </div>
+      <div className="container text-center">
+        <div className="card-body">
+          <h5 className="card-title text-primary">Add Discount</h5> <br />
+          <p className="card-text">
+            Administrative access to add discount information.
+          </p>
+          <Button
+            size="lg"
+            variant="outline-primary"
+            onClick={addDiscount}
+            className="me-2 d-flex justify-content-center container text-center"
+          >
+            Add Discount
+          </Button>
+        </div>
+      </div>
         <DropdownButton
           as={ButtonGroup}
           key="primary"
