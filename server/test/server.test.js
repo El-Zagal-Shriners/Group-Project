cf2cafb723d86a147f55e53adb5eeeb5b43f617e
Lloyd Prock <lloyd.prock@gmail.com>
2022-11-22 14:00:11 -0600

    setup server user test if authenticated

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -11,4 +11,16 @@ describe("Test the root path", () => {
     const response = await testServer(app).get("/api/user");
     expect(response.statusCode).toBe(403);
   });
+
+  test("Test if returning info when authenticated", async () => {
+    let agent = testServer.agent(app);
+    const response = await agent
+      .post("/api/user/login")
+      .send({ username: "admin", password: "1234" });
+    expect(response.statusCode).toBe(200);
+
+    const userResponse = await agent.get("/api/user");
+    expect(userResponse.statusCode).toBe(200);
+    userResponse && console.log("received userResponse");
+  });
 });
