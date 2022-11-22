046768e9cedaff47601d1649c691ab60a2c4b74c
Lloyd Prock <lloyd.prock@gmail.com>
2022-11-22 16:17:36 -0600

    setup working verification of admin middleware on member info

--- server/test/user.test.js
+++ server/test/user.test.js
@@ -15,14 +15,14 @@ describe("Test the root path", () => {
   test("Test if returning info when authenticated", async () => {
     let agent = testServer.agent(app);
     const response = await agent.post("/api/user/login").send({
-      username: process.env.TESTING_USERNAME,
-      password: process.env.TESTING_PASSWORD,
+      username: process.env.MEMBER_TESTING_USERNAME,
+      password: process.env.MEMBER_TESTING_PASSWORD,
     });
     expect(response.statusCode).toBe(200);
 
     const userResponse = await agent.get("/api/user");
     expect(userResponse.statusCode).toBe(200);
-    userResponse && console.log("received userResponse");
+    userResponse && console.log("received User Info");
   });
 
   // test("Test register with correct object", async () => {
@@ -40,4 +40,35 @@ describe("Test the root path", () => {
   //     .send(newUser);
   //   expect(response.statusCode).toBe(201);
   // });
+
+  test("Should receive 403 on get members without authorization/authentication", async () => {
+    const response = await testServer(app).get("/api/accounts/");
+    expect(response.statusCode).toBe(403);
+  });
+
+  test("Test if members return when authenticated and authorized", async () => {
+    let agent = testServer.agent(app);
+    const response = await agent.post("/api/user/login").send({
+      username: process.env.ADMIN_TESTING_USERNAME,
+      password: process.env.ADMIN_TESTING_PASSWORD,
+    });
+    expect(response.statusCode).toBe(200);
+
+    const userResponse = await agent.get("/api/accounts/");
+    expect(userResponse.statusCode).toBe(200);
+    userResponse && console.log("received Member Info");
+  });
+
+  test("Test if members return when authenticated but not authorized", async () => {
+    let agent = testServer.agent(app);
+    const response = await agent.post("/api/user/login").send({
+      username: process.env.MEMBER_TESTING_USERNAME,
+      password: process.env.MEMBER_TESTING_PASSWORD,
+    });
+    expect(response.statusCode).toBe(200);
+
+    const memberResponse = await agent.get("/api/accounts/");
+    expect(memberResponse.statusCode).toBe(403);
+    memberResponse && console.log("received Member Info");
+  });
 });
