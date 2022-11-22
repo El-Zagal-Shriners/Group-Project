81b3eaacf704630bfd13a00070c27f7a7cd4fd3c
David Vetter <ddvetter23@gmail.com>
2022-11-22 01:39:03 -0600

    formatted the new pages with prettier

--- src/components/PasswordResetPage/PasswordResetPage.jsx
+++ src/components/PasswordResetPage/PasswordResetPage.jsx
@@ -4,103 +4,115 @@ import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory, useParams } from "react-router-dom";
 
-function PasswordResetPage(){
-    const tokenCheck = useSelector((store) => store.tokenCheck.passwordResetTokenCheck);
-    const [newPassword, setNewPassword] = useState("");
-    const [confirmNewPassword, setConfirmNewPassword] = useState("");
-    const [showInvalid, setShowInvalid] = useState(false);
-    const [showValid, setShowValid] = useState(false);
-    const dispatch = useDispatch();
-    const { token } = useParams();
-    const history = useHistory();
-    // Checks if the token in the url is valid
-    useEffect(() => {
-        dispatch({
-          type: "RESET_PASSWORD_TOKEN_CHECK",
-          payload: token,
-        });
-        console.log('this is tokenCheck: ', tokenCheck);
-      }, []);
-    // function to submit the new password
-    const submitReset = (e) => {
-        e.preventDefault();
-        if(passwordCompare()){
-        dispatch({
-            type: "RESET_PASSWORD",
-            payload: {
-              password: newPassword,
-              token: token
-            }
-          });
-          history.push('/');
-        }
+function PasswordResetPage() {
+  const tokenCheck = useSelector(
+    (store) => store.tokenCheck.passwordResetTokenCheck
+  );
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmNewPassword, setConfirmNewPassword] = useState("");
+  const [showInvalid, setShowInvalid] = useState(false);
+  const [showValid, setShowValid] = useState(false);
+  const dispatch = useDispatch();
+  const { token } = useParams();
+  const history = useHistory();
+  // Checks if the token in the url is valid
+  useEffect(() => {
+    dispatch({
+      type: "RESET_PASSWORD_TOKEN_CHECK",
+      payload: token,
+    });
+    console.log("this is tokenCheck: ", tokenCheck);
+  }, []);
+  // function to submit the new password
+  const submitReset = (e) => {
+    e.preventDefault();
+    if (passwordCompare()) {
+      dispatch({
+        type: "RESET_PASSWORD",
+        payload: {
+          password: newPassword,
+          token: token,
+        },
+      });
+      history.push("/");
     }
-    // compare the two passwords
-    const passwordCompare = () => {
-        return newPassword===confirmNewPassword?true:false;
-    };
-    // toggle if password box should invalid or valid
-    // based on the two passwords entered
-    useEffect(() => {
-        // Sets both valid and invalid to false if only newPassword
-        // has entry or both or empty
-        if ((!newPassword && !confirmNewPassword) || (newPassword && !confirmNewPassword)){
-            setShowInvalid(false);
-            setShowValid(false);
-            return;
-        }
-        // show invalid when both passwords don't match
-        newPassword===confirmNewPassword?setShowInvalid(false):setShowInvalid(true);
-        // show valid when both passwords match
-        newPassword===confirmNewPassword?setShowValid(true):setShowValid(false);
-    }, [confirmNewPassword, newPassword]);
-    return (
-        <form
-        className="d-flex flex-column align-items-center p-5 rounded-3 border border-2 border-primary shadow-lg mb-3"
-        onSubmit={submitReset}
-      >
-        <h2 className="text-primary">Reset Password</h2>
-        {tokenCheck==='true' && <h3>VALID</h3>}
-        <div>
-          <FloatingLabel
-            controlId="newPasswordInput"
-            label="New Password"
-            className={`mb-1 text-primary`}
-          >
-            <Form.Control
-              type="password"
-              name="newPassword"
-              value={newPassword}
-              placeholder="New Password"
-              required
-              onChange={(event) => setNewPassword(event.target.value)}
-            />
-          </FloatingLabel>
-  
-          <FloatingLabel
-            controlId="floatingFirstName"
-            label="Confirm New Password"
-            className="mb-1 text-primary"
-          >
-            <Form.Control
-              type="password"
-              name="confirmNewPassword"
-              value={confirmNewPassword}
-              placeholder="Confirm New Password"
-              required
-              isInvalid={showInvalid?true:false}
-              isValid={showValid?true:false}
-              onChange={(event) => setConfirmNewPassword(event.target.value)}
-            />
-          </FloatingLabel>
-        </div>
-          <button
-            className="btn btn-primary"
-            type="submit">
-                Submit
+  };
+  // compare the two passwords
+  const passwordCompare = () => {
+    return newPassword === confirmNewPassword ? true : false;
+  };
+  // toggle if password box should invalid or valid
+  // based on the two passwords entered
+  useEffect(() => {
+    // Sets both valid and invalid to false if only newPassword
+    // has entry or both or empty
+    if (
+      (!newPassword && !confirmNewPassword) ||
+      (newPassword && !confirmNewPassword)
+    ) {
+      setShowInvalid(false);
+      setShowValid(false);
+      return;
+    }
+    // show invalid when both passwords don't match
+    newPassword === confirmNewPassword
+      ? setShowInvalid(false)
+      : setShowInvalid(true);
+    // show valid when both passwords match
+    newPassword === confirmNewPassword
+      ? setShowValid(true)
+      : setShowValid(false);
+  }, [confirmNewPassword, newPassword]);
+  return (
+    <form
+      className="d-flex flex-column align-items-center p-5 rounded-3 border border-2 border-primary shadow-lg mb-3"
+      onSubmit={submitReset}
+    >
+      <h2 className="text-primary">Reset Password</h2>
+      {tokenCheck === "true" ? (
+        <>
+          <div>
+            <FloatingLabel
+              controlId="newPasswordInput"
+              label="New Password"
+              className={`mb-1 text-primary`}
+            >
+              <Form.Control
+                type="password"
+                name="newPassword"
+                value={newPassword}
+                placeholder="New Password"
+                required
+                onChange={(event) => setNewPassword(event.target.value)}
+              />
+            </FloatingLabel>
+
+            <FloatingLabel
+              controlId="floatingFirstName"
+              label="Confirm New Password"
+              className="mb-1 text-primary"
+            >
+              <Form.Control
+                type="password"
+                name="confirmNewPassword"
+                value={confirmNewPassword}
+                placeholder="Confirm New Password"
+                required
+                isInvalid={showInvalid ? true : false}
+                isValid={showValid ? true : false}
+                onChange={(event) => setConfirmNewPassword(event.target.value)}
+              />
+            </FloatingLabel>
+          </div>
+          <button className="btn btn-primary" type="submit">
+            Submit
           </button>
-      </form>
-    )
+        </>
+      ) : (
+        <h4>This link is invalid. Make sure to use the link from the email.</h4>
+      )}
+    </form>
+  );
 }
 
 export default PasswordResetPage;
--- src/components/RequestPasswordReset/RequestPasswordReset.jsx
+++ src/components/RequestPasswordReset/RequestPasswordReset.jsx
@@ -1,60 +1,69 @@
-import { useState } from 'react';
-import Modal from 'react-bootstrap/Modal';
-import Button from 'react-bootstrap/Button';
-import { useDispatch }  from "react-redux";
-import FloatingLabel from 'react-bootstrap/FloatingLabel';
-import Form from 'react-bootstrap/Form';
+import { useState } from "react";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import { useDispatch } from "react-redux";
+import FloatingLabel from "react-bootstrap/FloatingLabel";
+import Form from "react-bootstrap/Form";
 
 function RequestPasswordReset(props) {
-    // local state to hold the dependent email address
-    const [email, setEmail] = useState('');
-    const dispatch = useDispatch();
-    // performs POST for sending email to the email the user supplies
-    const sendResetEmail = (e) => {
-        dispatch({
-            type: "SEND_RESET_PASSWORD_EMAIL",
-            payload: {
-              email
-            }
-        });
-        // clear local state
-        setEmail('');
-        // close the add reset password email modal
-        props.handleCloseResetPassword(e);
-    }
-    // clears local state and closes modal
-    const cancelRequestPassword = (e) => {
-        setEmail('');
-        props.handleCloseResetPassword(e);
-    }
+  // local state to hold the dependent email address
+  const [email, setEmail] = useState("");
+  const dispatch = useDispatch();
+  // performs POST for sending email to the email the user supplies
+  const sendResetEmail = (e) => {
+    dispatch({
+      type: "SEND_RESET_PASSWORD_EMAIL",
+      payload: {
+        email,
+      },
+    });
+    // clear local state
+    setEmail("");
+    // close the add reset password email modal
+    props.handleCloseResetPassword(e);
+  };
+  // clears local state and closes modal
+  const cancelRequestPassword = (e) => {
+    setEmail("");
+    props.handleCloseResetPassword(e);
+  };
 
-    return (
-        <Modal show={props.showResetPassword} onHide={cancelRequestPassword}>
-        <Modal.Header className="bg-primary">
-          <Modal.Title className="fw-bold text-light">Password Reset</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
+  return (
+    <Modal show={props.showResetPassword} onHide={cancelRequestPassword}>
+      <Modal.Header className="bg-primary">
+        <Modal.Title className="fw-bold text-light">Password Reset</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
         <>
-        Please enter your email address.
-        <FloatingLabel
+          Please enter your email address.
+          <FloatingLabel
             controlId="dependentEmailLabel"
             label="Email"
             className="mb-3"
-        >
-        <Form.Control required type="text" placeholder="Email" value={email} onChange={(e)=>setEmail(e.target.value)}/>
-        </FloatingLabel>
+          >
+            <Form.Control
+              required
+              type="text"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </FloatingLabel>
         </>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="outline-primary" onClick={(e)=>cancelRequestPassword(e)}>
-            Cancel
-          </Button>
-          <Button variant="primary" onClick={(e)=>sendResetEmail(e)}>
-            Send Email
-          </Button>
-        </Modal.Footer>
-        </Modal>
-    )
+      </Modal.Body>
+      <Modal.Footer>
+        <Button
+          variant="outline-primary"
+          onClick={(e) => cancelRequestPassword(e)}
+        >
+          Cancel
+        </Button>
+        <Button variant="primary" onClick={(e) => sendResetEmail(e)}>
+          Send Email
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
 }
 
 export default RequestPasswordReset;
