ca5700ec4c069be6b0a5cd11f6417cb881d0398b
David Vetter <ddvetter23@gmail.com>
2022-11-24 10:28:26 -0600

    added body and light styling to request review page

--- src/components/RequestReviewPage/RequestReviewPage.jsx
+++ src/components/RequestReviewPage/RequestReviewPage.jsx
@@ -1,10 +1,19 @@
 import {useHistory} from 'react-router-dom';
+import UpdatedNavBar from '../Nav/Nav';
 function RequestReviewPage() {
     const history = useHistory();
+    const requestReview = () => {
+        return;
+    }
     return (
-        <div>
-        <h1>Your account is currently turned off</h1>
-        <button onClick={()=>history.push('/user')}>Profile</button>
+        <div className='w-100'>
+            <UpdatedNavBar />
+            <div className="container col col-lg-6 d-flex flex-column align-items-center p-3 rounded-3 shadow-lg mb-3 bg-primary">
+                <h1 className="text-light fw-bold">Inactive Status</h1>
+                <h4 className="fw-bold text-secondary">Request Review</h4>
+                <p className="border border-2 border-primary p-3 rounded bg-light">Your account may be turned off for past dues or other reasons. If you recently made a payment or have another reason your account should be re-authorized, please use the button below to request your account status be reviewed.</p>
+                <button className="shadow-md btn btn-primary border border-light" onClick={requestReview}>Request Review</button>
+            </div>
         </div>
     )
 }
