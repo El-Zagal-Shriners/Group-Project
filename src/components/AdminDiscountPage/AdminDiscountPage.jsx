ad132db1247c41210dd86a0e6329b4747e0a1e98
David Vetter <ddvetter23@gmail.com>
2022-11-26 17:09:13 -0600

    adjusted sizing on discount manager legend chips

--- src/components/AdminDiscountPage/AdminDiscountPage.jsx
+++ src/components/AdminDiscountPage/AdminDiscountPage.jsx
@@ -129,13 +129,17 @@ function AdminDiscountPage(vendor) {
       <section className="col col-md-9 col-lg-6 w-100">
         <hr className="mt-2 mb-0 col col-md-9 col-lg-6 mx-auto"/>
       <div className="w-lg-50 w-md-100 mx-auto col col-md-9 col-lg-6 d-flex justify-content-center align-items-center mt-2 mb-0">
-          <div className="col p-2 d-flex justify-content-between align-items-center">
+          <div className="col p-2 d-flex justify-content-around align-items-center">
             <p className="col text-nowrap m-0">Active:</p>
+            <div className="w-100 d-flex justify-content-center mx-3">
             <p className="col badge bg-primary m-0">&nbsp;</p>
+            </div>
           </div>
-          <div className="col p-2 d-flex justify-content-between align-items-center">
+          <div className="col p-2 d-flex justify-content-around align-items-center">
             <p className="col text-nowrap m-0">In-active:</p>
-            <p className="col border border-1 border-primary bg-light badge m-0">&nbsp;</p>
+            <div className="w-100 d-flex justify-content-center mx-3">
+            <p className="col mx-auto border border-1 border-primary bg-light badge m-0">&nbsp;</p>
+            </div>
           </div>
         </div>
         <hr className="my-2 mx-auto col col-md-9 col-lg-6"/>
