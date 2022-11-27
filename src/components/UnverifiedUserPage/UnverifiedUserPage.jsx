43b984a7af63e9e54132ba5e3fa38abe4c204a2e
Lloyd Prock <lloyd.prock@gmail.com>
2022-11-26 23:34:56 -0600

    updated initial verification landing page

--- src/components/UnverifiedUserPage/UnverifiedUserPage.jsx
+++ src/components/UnverifiedUserPage/UnverifiedUserPage.jsx
@@ -1,18 +1,21 @@
 import UpdatedNavBar from "../Nav/Nav";
 
 function UnverifiedUserPage() {
-    return (
-      <div>
-        <UpdatedNavBar />
-        <div className="container col col-lg-6 d-flex flex-column align-items-center p-3 rounded-3 shadow-lg mb-3 bg-primary">
-          <h2 className="text-light fw-bold text-center">New Account Verification</h2>
+  return (
+    <div>
+      <UpdatedNavBar />
+      <div className="container col col-lg-6 d-flex flex-column align-items-center p-3 h-100">
+        <div className="text-light text-center bg-primary w-100 m-0 py-2 mb-1 rounded-2">
+          <h2 className="fw-bold">New Account Verification</h2>
           <h4 className="fw-bold text-secondary">Verification Pending</h4>
-          <p className="border border-2 border-primary p-3 rounded bg-light">
-            Your new account is still under intial verification. This process may take a couple days to complete.
-          </p>
         </div>
+        <p className="border border-2 border-primary p-3 rounded bg-white text-center">
+          Your new account is still under initial verification. This process may
+          take a couple days to complete.
+        </p>
       </div>
-    );
+    </div>
+  );
 }
 
 export default UnverifiedUserPage;
