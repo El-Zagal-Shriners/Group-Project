5861b4e892f23c8a13ad80a41a68dd273bd2db29
Lee Baesler <lee.baesler@gmail.com>
2022-11-27 20:40:10 -0600

    working on admin page

--- src/components/FaqAdmin.jsx/FaqAdmin.jsx
+++ src/components/FaqAdmin.jsx/FaqAdmin.jsx
@@ -15,9 +15,10 @@ function FaqAdmin() {
           </Accordion.Header>
           <Accordion.Body>
             <div className="text-primary">
-            Yes! By clicking on 'Manage Members', the Admin can approve new
-            member's access, activate or deactive members and their dependents,
-            and view a list of all members, dependents, and their status!
+              Yes! By clicking on 'Manage Members', the Admin can approve new
+              member's access, activate or deactive members and their
+              dependents, and view a list of all members, dependents, and their
+              status!
             </div>
           </Accordion.Body>
         </Accordion.Item>
@@ -29,37 +30,45 @@ function FaqAdmin() {
           </Accordion.Header>
           <Accordion.Body>
             <div className="text-primary">
-            Once inside 'Manage Discounts', the Admin simply clicks on "Add
-            Discount", selects the vendor's name from the dropdown, inputs the
-            discount information on the form, selects what category the discount
-            belongs in and clicks "Add Discount"
+              Once inside 'Manage Discounts', the Admin simply clicks on "Add
+              Discount", selects the vendor's name from the dropdown, inputs the
+              discount information on the form, selects what category the
+              discount belongs in and clicks "Add Discount"
             </div>
           </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey="2">
           <Accordion.Header>
-            <div className="text-primary fw-bold">
-              Can an Admin delete or change a current discount?
-            </div>
+            <div className="text-primary fw-bold">Can I remove a discount?</div>
           </Accordion.Header>
           <Accordion.Body>
             <div className="text-primary">
-            You know it! By simply clicking on the discount, the Admin can edit,
-            delete, and even turn off the discount if they want to keep its use
-            for later!
+              You know it! By simply clicking on the discount, the Admin can
+              edit, delete, and even turn off the discount if they want to keep
+              its use for later!
             </div>
           </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey="3">
           <Accordion.Header>
-          <div className="text-primary fw-bold">
-            What if a Vendor no longer wants to partner with Shriners?
-            </div>
+            <div className="text-primary fw-bold">Can I remove a vendor?</div>
           </Accordion.Header>
           <Accordion.Body>
             <div className="text-primary">
-            The Admin can simply click on that vendors name and remove that
-            vendors information along with its discounts.
+              The Admin can simply click on that vendors name and remove that
+              vendors information along with its discounts.
+            </div>
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="3">
+          <Accordion.Header>
+            <div className="text-primary fw-bold">Are disconts tracked?</div>
+          </Accordion.Header>
+          <Accordion.Body>
+            <div className="text-primary">
+              Absolutely! The admin can view how many times a discount has been
+              revealed to a member in the last 7 days, past month, past yeat,
+              and all-time!
             </div>
           </Accordion.Body>
         </Accordion.Item>
